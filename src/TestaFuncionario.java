40e8d9e6cad8ab60546771986bbd626c51bfb3d1
Paulo Henrique <paulokenza@gmail.com>
2018-11-22 09:29:32 -0200

    Add Funcionario e Gerente classes

---
+++ src/TestaFuncionario.java
@@ -0,0 +1,18 @@
+
+public class TestaFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		
+		nico.setNome("Nico Sepat");
+		nico.setCpf("121121121-08");
+		nico.setSalario(2000.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+		
+	}
+
+}
